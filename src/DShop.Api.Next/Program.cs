f6d8f005b006851d2383b90b4ac2ddb55d1d4c74
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-02-24 13:42:05 +0100

    Updated Ntrada package

--- src/DShop.Api.Next/Program.cs
+++ src/DShop.Api.Next/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using NGate;
+using Ntrada;
+using Ntrada.Extensions.RabbitMq;
 
 namespace DShop.Api.Next
 {
     public static class Program
     {
         public static async Task Main(string[] args)
-            => await WebHost.CreateDefaultBuilder(args).UseNGate().Build().RunAsync();
+            => await WebHost.CreateDefaultBuilder(args)
+                .UseNtrada()
+                .UseRabbitMq()
+                .Build()
+                .RunAsync();
     }
 }
